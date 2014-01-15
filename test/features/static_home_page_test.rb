8c07567f7de6d4eadca2717a183f22b7ec77d163
Margaret Morris <margaretblue@gmail.com>
2014-01-14 21:40:16 -0800

    tests passing, basic index page added

--- test/features/static_home_page_test.rb
+++ test/features/static_home_page_test.rb
@@ -3,7 +3,6 @@ require "test_helper"
 feature "StaticHomePage" do
   scenario "the test is sound" do
     visit root_path
-    page.must_have_content "Hello World"
     page.wont_have_content "Goobye All!"
   end
 end
